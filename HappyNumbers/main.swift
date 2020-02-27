08d66d95e3e805ad22fde1549f912e0b8cf48418
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-02-27 10:38:33 +0100

    Initial Commit

---
+++ HappyNumbers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HappyNumbers
+//
+//  Created by Heiko Goes on 27.02.20.
+//  Copyright © 2020 Heiko Goes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
